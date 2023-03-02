d67081229aab64724eeb301b57403a1a18943aef
Zakelyro <107665015+Zakelyro@users.noreply.github.com>
2023-03-02 20:38:15 +0530

    Add files via upload

---
+++ passkeys.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+# Password making T1 
+# T1==4 letters and encoding
+L1=['Gauv','Isei','Geas','Dick','Cock','Lugh','KenK','KIRA','IITB','IITK','Porn','Solo','Abys','Sieg','Lily','Jack','Jily','Mark','Fuck','Fate','Zero','Lelu','Time','Dura','IDFC','Doge','Elon','Plat','Phil','BioB','LilG','Tore','Pika','Char','GenG','Giga']
+print(len(L1))
